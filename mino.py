278aa27a693a5b035f79d8b56c9927426e205885
fel97048 <fel97048@gmail.com>
2019-12-14 11:37:39 +0900

    mino.html追加、executeをFunctions用に修正

--- mino.py
+++ mino.py
@@ -12,7 +12,25 @@ class MouIkutsuNerutoOshogatsu:
 
         return delta.days
 
-def execute():
+def execute(request):
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
     mino = MouIkutsuNerutoOshogatsu()
     return str(mino.answer())
 
