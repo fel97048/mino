7d281a2e616f62a44f6bd727465f9595eb85a98b
fel97048 <fel97048@gmail.com>
2022-09-28 01:36:51 +0900

    app.pyで起動するときのポートを修正

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def index():
     return render_template('mino.html', answer = h)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
